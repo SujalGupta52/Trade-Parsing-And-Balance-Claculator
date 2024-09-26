4a8e3fda505192f9834546d6014a47cae0c3e005
Sujal Gupta <67575635+SujalGupta52@users.noreply.github.com>
2024-09-26 20:39:39 +0530

    Create route handler for update-database route

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
+import mongoose from "mongoose";
+import updateRouter from "./routes/updataRouter.js";
 
 dotenv.config();
 
@@ -12,6 +14,22 @@ app.use(cors());
 app.use(morgan("combined"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// database setup
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.DATABASE_URL;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+app.use("/update-database", updateRouter);
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(err.status || 500).send({ message: "An error occurred" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ src/routes/updataRouter.js
@@ -0,0 +1,36 @@
+import { Router } from "express";
+import multer from "multer";
+import fs from "fs";
+import csvParser from "csv-parser";
+import Trade from "../models/Trade.js";
+
+const updateRouter = Router();
+const upload = multer({ dest: "uploads/" });
+
+updateRouter.post("/", upload.single("file"), (req, res, next) => {
+  const results = [];
+  fs.createReadStream(req.file.path)
+    .pipe(csvParser())
+    .on("data", (data) => {
+      const [BaseCoin, QuoteCoin] = data.Market.split("/");
+      results.push({
+        UTC_Time: new Date(data.UTC_Time),
+        Operation: data.Operation.toUpperCase(),
+        Market: data.Market,
+        BaseCoin,
+        QuoteCoin,
+        Amount: parseFloat(data["Buy/Sell Amount"]),
+        Price: parseFloat(data.Price),
+      });
+    })
+    .on("end", async () => {
+      await Trade.insertMany(results);
+      res.json({ message: "File successfully uploaded and data stored." });
+    })
+    .on("error", (err) => {
+      console.log(err);
+      next(err);
+    });
+});
+
+export default updateRouter;
